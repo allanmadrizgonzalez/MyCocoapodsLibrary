c9197a5770ce43c50f0183f42ab3f590954b20ad
Allan <amadrizg@gmail.com>
2021-06-19 20:19:37 -0600

    first commit

---
+++ MyCocoapodsLibrary/Classes/Logger.swift
@@ -0,0 +1,23 @@
+//
+//  Logger.swift
+//  MyCocoapodsLibrary
+//
+//  Created by Allan Madriz on 6/19/21.
+//
+
+import Foundation
+
+public class Logger{
+    
+   public init() {
+    }
+    
+    public func printLog(){
+        print("Hola Mundo")
+    }
+    
+    func privateMethod(){
+        print("privado")
+    }
+    
+}
